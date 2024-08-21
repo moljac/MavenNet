295aae1014b9446bfeb5df5c03959e382333a0c8
moljac <mcvjetko@holisticware.net>
2024-08-21 16:15:36 +0200

    unit tests

--- MavenNet.Tests/MavenVersioningTests.cs
+++ MavenNet.Tests/MavenVersioningTests.cs
@@ -18,6 +18,14 @@ namespace MavenNet.Tests
 		[InlineData("(,1.0],[1.2,)", "1.2", true)]
 		[InlineData("(,1.0],[1.2,)", "1.3", true)]
 		[InlineData("(,1.0],[1.2,)", "1.1", false)]
+		[InlineData("(,1.0],[2.2,)", "1.0.1.262e11d", false)]
+		[InlineData("(,0.8],[2.2,)", "0.11.1.647c3c2", false)]
+		[InlineData("(,0.8],[2.2,)", "0.11.1.3c786d2", false)]
+		[InlineData("(,0.8],[2.2,)", "0.10.1.2e8fe11", false)]
+		[InlineData("(,0.8],[2.2,)", "0.10.1.e92f734", false)]
+		[InlineData("(,0.8],[2.2,)", "0.10.1.d22d4de", false)]
+		[InlineData("(,0.8],[2.2,)", "0.9.3.545a756", false)]
+		[InlineData("(,0.8],[2.2,)", "0.9.3.a319893", false)]
 		public void Satisfy_Versions(string range, string version, bool satisfies)
 		{
 			var mvr = new MavenVersionRange(range);
